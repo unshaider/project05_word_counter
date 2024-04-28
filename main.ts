ef1c98fba2140013120817778c6ce589a58bb6cc
Syed Uns Haider Zaidi <unshaider28@gmail.com>
2024-04-29 01:07:30 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let answers = await inquirer.prompt([{
+    name: "counter",
+    type: "input",
+    message: "Write the sentence to count the words:",
+}])
+
+const whiteSpace = answers.counter.trim().split(" ")
+console.log(whiteSpace);
+console.log(chalk.yellow(`The total words in your sentence are ${whiteSpace.length}.`));
